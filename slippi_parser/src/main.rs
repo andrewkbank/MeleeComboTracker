d04ce8fc0fcd04e04496bd0919caf8b89722cfb2
abank <banka@rpi.edu>
2024-03-31 20:53:31 -0400

    all 10 fields from the google doc added

--- slippi_parser/src/main.rs
+++ slippi_parser/src/main.rs
@@ -11,8 +11,9 @@ use ssbm_data::action_state::Common::{*};
 //stole a lot from https://github.com/project-slippi/slippi-js/blob/master/src/stats/combos.ts#L7
 
 fn main() {
-    let mut r = io::BufReader::new(fs::File::open("tests/test3.slp").unwrap());
+    let mut r = io::BufReader::new(fs::File::open("tests/test4.slp").unwrap());
     let game = read(&mut r, None).unwrap();
+    /*
     let metadata = game.metadata;
     match metadata {
         Some(map) => {
@@ -25,43 +26,39 @@ fn main() {
             println!("Option is None");
         }
     }
+    */
+    let stage = game.start.stage;
     let rollbacks = game.frames.rollbacks(Rollbacks::ExceptLast);
     let mut last_attack: [u8;2] = [0,0];
+    let mut start_frame: [usize;2] = [0,0];
     for frame_idx in 1..game.frames.len() {
         if rollbacks[frame_idx]{
             continue;
         }
         for (port_idx, port_data) in game.frames.ports.iter().enumerate() {
-            //let damage_taken = port_data.leader.post.percent.get(frame_idx).unwrap_or(0.0) - port_data.leader.post.percent.get(frame_idx-1).unwrap_or(0.0);
-
             let state=port_data.leader.post.state.get(frame_idx).unwrap_or(0);
 
             let last_hit_by_instance = <Option<arrow2::array::PrimitiveArray<u16>> as Clone>::clone(&port_data.leader.post.last_hit_by_instance).unwrap_or_default().get(frame_idx-1).unwrap_or(0);
             let hit_by_instance = <Option<arrow2::array::PrimitiveArray<u16>> as Clone>::clone(&port_data.leader.post.last_hit_by_instance).unwrap_or_default().get(frame_idx).unwrap_or(0);
-            
-            //let position = port_data.leader.post.position;
 
             let last_state=port_data.leader.post.state.get(frame_idx-1).unwrap_or(0);
             let in_hitstun= is_damaged(last_state)||is_grabbed(last_state)||is_command_grabbed(last_state);
+            let character = port_data.leader.post.character.get(frame_idx).unwrap_or(0);
+            let opp_character = game.frames.ports[((port_idx as u8+1)%2) as usize].leader.post.character.get(frame_idx).unwrap_or(0);
 
             //track getting grabbed
             if is_grabbed(state) && !is_grabbed(port_data.leader.post.state.get(frame_idx-1).unwrap_or(0)){   //grabbed this frame and not the last frame
                 if in_hitstun {
-                    println!(
-                        "{} combo grabbed on frame {}", 
-                        game.start.players[port_idx].port,
-                        game.frames.id.get(frame_idx).unwrap()
-                    );
-                    println!(
-                        "{} comboed into grab",
-                        last_attack[port_idx]
-                    );
+                    println!("{} combo grabbed on frame {}", game.start.players[port_idx].port, game.frames.id.get(frame_idx).unwrap());
+                    println!("Start position: ({},{}), End position: ({},{})", port_data.leader.post.position.x.get(start_frame[port_idx]).unwrap_or(0.0),port_data.leader.post.position.y.get(start_frame[port_idx]).unwrap_or(0.0),port_data.leader.post.position.x.get(frame_idx).unwrap_or(0.0),port_data.leader.post.position.y.get(frame_idx).unwrap_or(0.0));
+                    println!("{} comboed into grab", last_attack[port_idx]);
+                    println!("character {} comboed by {}",character,opp_character);
+                    println!("Start %: {}, End %: {}",port_data.leader.post.percent.get(start_frame[port_idx]-1).unwrap_or(0.0),port_data.leader.post.percent.get(frame_idx).unwrap_or(0.0));
+                    println!("Frames between moves: {}",(frame_idx-start_frame[port_idx]));
+                    println!("Stage: {}",stage);
+                    println!("---------------");
                 }else{
-                    println!(
-                        "{} raw grabbed on frame {}", 
-                        game.start.players[port_idx].port,
-                        game.frames.id.get(frame_idx).unwrap()
-                    );
+                    println!("{} raw grabbed on frame {}", game.start.players[port_idx].port, game.frames.id.get(frame_idx).unwrap());
                 }
                 
             }else 
@@ -83,18 +80,17 @@ fn main() {
                         opponent=0;
                     }
                 }
-                //let opponent_move = game.frames.ports[opponent as usize].leader.post.state.get(frame_idx).unwrap_or(0);
                 let opponent_attack= game.frames.ports[opponent as usize].leader.post.last_attack_landed.get(frame_idx).unwrap_or(0);
                 if !is_pummel_or_throw(opponent_attack) {
                     if in_hitstun{
-                        println!(
-                            "{} comboed on frame {} by instance {}", game.start.players[port_idx].port, game.frames.id.get(frame_idx).unwrap(), hit_by_instance
-                        );
-                        println!(
-                            "{} comboed into {}",
-                            last_attack[port_idx],
-                            opponent_attack
-                        );
+                        println!("{} comboed on frame {} by instance {}", game.start.players[port_idx].port, game.frames.id.get(frame_idx).unwrap(), hit_by_instance);
+                        println!("Start position: ({},{}), End position: ({},{})", port_data.leader.post.position.x.get(start_frame[port_idx]).unwrap_or(0.0),port_data.leader.post.position.y.get(start_frame[port_idx]).unwrap_or(0.0),port_data.leader.post.position.x.get(frame_idx).unwrap_or(0.0),port_data.leader.post.position.y.get(frame_idx).unwrap_or(0.0));
+                        println!("{} comboed into {}",last_attack[port_idx],opponent_attack);
+                        println!("character {} comboed by {}",character,opp_character);
+                        println!("Start %: {}, End %: {}",port_data.leader.post.percent.get(start_frame[port_idx]-1).unwrap_or(0.0) as u16,port_data.leader.post.percent.get(frame_idx).unwrap_or(0.0) as u16);
+                        println!("Frames between moves: {}",(frame_idx-start_frame[port_idx]));
+                        println!("Stage: {}",stage);
+                        println!("---------------");
                     }else{
                         println!(
                             "{} raw hit on frame {} by instance {}", 
@@ -105,6 +101,7 @@ fn main() {
                     }
                 }
                 last_attack[port_idx]=opponent_attack;
+                start_frame[port_idx]=frame_idx;
             }
         }
     }
