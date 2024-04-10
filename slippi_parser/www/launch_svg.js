243ad8cf95593821942f5b955c81c3efded704dd
luchsj <luchsj@rpi.edu>
2024-04-10 15:09:02 -0400

    Move inline JS to external files.

---
+++ slippi_parser/www/launch_svg.js
@@ -0,0 +1,34 @@
+// Fetch the given SVG file and load it into the page.
+var svg = document.getElementById("svgobject");
+
+svg.addEventListener("load", function(){
+  var svgDoc = svg.contentDocument.querySelectorAll('svg');
+  var nodes = svgDoc[0].querySelectorAll('path');
+
+  for( var i = 0; i < nodes.length; i++ ) {
+    nodes[i].addEventListener('click', changeStroke);
+    //TODO: add listener to reset strokecolor.
+  }
+  console.log(parsed);
+  var lastPathClicked = null;
+
+  function changeStroke() {
+    this.setAttribute('opacity', 0.30);
+    this.setAttribute('stroke', 'red');
+    if(lastPathClicked)
+    {
+      lastPathClicked.setAttribute('opacity', 0.10);
+      lastPathClicked.setAttribute('stroke', 'black');
+    }
+    lastPathClicked = this;
+
+    const id = this.getAttribute("id");
+    const start_percent = parsed[id][8];
+    const end_percent = parsed[id][9];
+    const text =
+      "Start percent: " + start_percent + "\n" +
+      "End percent: " + end_percent + "\n";
+
+    document.getElementById("info").innerText = text;
+  }
+})
