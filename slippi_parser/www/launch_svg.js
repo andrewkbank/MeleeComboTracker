c7aa973201a50779cc7db75cd2fa5fa44cf1c7f8
luchsj <luchsj@rpi.edu>
2024-04-12 14:48:35 -0400

    Zoom and pan functional. Must click on floating info box. Zoom with scroll, pan with arrow keys.

--- slippi_parser/www/launch_svg.js
+++ slippi_parser/www/launch_svg.js
@@ -1,5 +1,33 @@
 // Fetch the given SVG file and load it into the page.
 var svg = document.getElementById("svgobject");
+const characters = [
+  "Captain Falcon",
+	"Donkey Kong",
+	'Fox',
+	'Mr. Game And Watch',
+	'Kirby',
+	'Bowser',
+	'Link',
+	'Luigi',
+	'Mario',
+	'Marth',
+	'Mewtwo',
+	'Ness',
+	'Peach',
+	'Pikachu',
+	'Ice Climbers',
+	'Jigglypuff',
+	'Samus',
+	'Yoshi',
+	'Zelda',
+	'Sheik',
+	'Falco',
+	'Young Link',
+	'Dr. Mario',
+	'Roy',
+	'Pichu',
+	'Ganondorf',
+];
 
 svg.addEventListener("load", function(){
   var svgDoc = svg.contentDocument.querySelectorAll('svg');
@@ -12,11 +40,26 @@ svg.addEventListener("load", function(){
   console.log(parsed);
   var lastPathClicked = null;
 
-  panzoom = Panzoom(svg);
+  const zoomable = document.getElementById("zoomable");
+  panzoom = Panzoom(zoomable);
+  panzoom.bind();
   if(panzoom)
     console.log("Panzoom successfully initialized.");
-  svg.parentElement.addEventListener('wheel', panzoom.zoomWithWheel);
-
+  document.addEventListener('wheel', panzoom.zoomWithWheel);
+  document.addEventListener(
+  "keydown",
+  (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+    if(keyName === 'ArrowLeft')
+      panzoom.pan(10, 0, {relative:true});
+    if(keyName === 'ArrowRight')
+      panzoom.pan(-10, 0, {relative:true});
+    if(keyName === 'ArrowUp')
+      panzoom.pan(0, 10, {relative:true});
+    if(keyName === 'ArrowDown')
+      panzoom.pan(0, -10, {relative:true});
+  });
   function changeStroke() {
     this.setAttribute('opacity', 0.30);
     this.setAttribute('stroke', 'red');
@@ -30,7 +73,9 @@ svg.addEventListener("load", function(){
     const id = this.getAttribute("id");
     const start_percent = parsed[id][8];
     const end_percent = parsed[id][9];
-    const text =
+    const text = 
+      'Attacker: ' + characters[parsed[id][6]] + '\n' +
+      'Defender: ' + characters[parsed[id][7]] + '\n' +
       "Start percent: " + start_percent + "\n" +
       "End percent: " + end_percent + "\n";
 
